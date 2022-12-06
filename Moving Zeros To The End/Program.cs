cc489585e3603f82853f6ca40d3b4b1805fb0649
Kraichikk <108270237+Kraichikk@users.noreply.github.com>
2022-12-06 14:14:05 +0200

    Add project files.

---
+++ Moving Zeros To The End/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace Moving_Zeros_To_The_End
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1 };
+
+            int[] result = MyMethod(arr);
+
+            foreach (var n in result)
+            {
+                Console.WriteLine(n);
+            }
+        }
+        static int[] MyMethod(int[] arr)
+        {
+            int[] array = new int[arr.Length];
+            Array.Fill<int>(array, 0);
+
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 0)
+                    count++;
+
+                if (arr[i] != 0)
+                    array[i - count] = arr[i];
+            }
+            return array;
+        }
+        //more clever solution
+        public static int[] MoveZeroes(int[] arr)
+        {
+            return arr.OrderBy(x => x == 0).ToArray();
+        }
+    }
+}
